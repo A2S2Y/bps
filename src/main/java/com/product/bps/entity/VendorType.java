16ab94096e5be60e8db99d29f2f44efbd73956c7
PadminiP-Hexaware <PadminiP@hexaware.com>
2018-05-28 09:15:44 +0530

    vendor type entity class

---
+++ src/main/java/com/product/bps/entity/VendorType.java
@@ -0,0 +1,22 @@
+package com.product.bps.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vendor_type")
+public class VendorType {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int vendorTypeId;
+
+	@Column(name = "vendor_type_name")
+	private String vendorTypeName;
+
+	@Column(name = "amount")
+	private float amount;
+}
--- src/main/java/com/product/bps/repository/VendorRepository.java
+++ src/main/java/com/product/bps/repository/VendorRepository.java
@@ -1,9 +1,11 @@
 package com.product.bps.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.product.bps.entity.Vendor;
 
+@Repository
 public interface VendorRepository extends JpaRepository<Vendor,Long> {
 
 }
